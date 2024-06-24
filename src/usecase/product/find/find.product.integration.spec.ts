84b257ae886c2ed8fb0aafdf5349a12bf5746644
mvrdgs <mauricio.viegas@mercadolivre.com>
2024-06-24 19:07:09 -0300

    add product usecase

---
+++ src/usecase/product/find/find.product.integration.spec.ts
@@ -0,0 +1,60 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import FindProductUseCase from "./find.product.usecase";
+
+describe("Integration Test find product use case", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should find a product", async () => {
+        const productRepository = new ProductRepository();
+        const usecase = new FindProductUseCase(productRepository);
+
+        const product = new Product("123", "Product", 99);
+
+        await productRepository.create(product);
+
+        const input = {
+            id: "123",
+        };
+
+        const expected = {
+            id: "123",
+            name: "Product",
+            price: 99,
+        };
+
+        const result = await usecase.execute(input);
+
+        expect(result).toEqual(expected);
+    });
+
+    it("should not find a product", async () => {
+        const productRepository = new ProductRepository();
+        const usecase = new FindProductUseCase(productRepository);
+
+        const input = {
+            id: "123",
+        };
+
+        await expect(() => {
+            return usecase.execute(input);
+        }).rejects.toThrow("Product not found")
+    });
+});
